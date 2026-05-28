d6588bb25d0e16544d4c8b5697579972b0d21d56
rjahir-rv <rjahir.rv@gmail.com>
2026-05-28 17:36:43 -0600

    Configure CI/CD automation and update SDK to 37

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+This project uses [Release Please](https://github.com/googleapis/release-please) and Conventional Commits to generate release notes.
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,17 +6,17 @@ plugins {
 android {
     namespace = "com.catlytics.app"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
+        version = release(37) {
+            minorApiLevel = 0
         }
     }
 
     defaultConfig {
         applicationId = "com.catlytics.app"
         minSdk = 28
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
